c1fa256eb684838bfa7cd1c57e0d5158d045b633
Mahmoud Malhis <112867127+MahmoudMalhis@users.noreply.github.com>
2025-08-11 23:25:42 +0300

    Update NotificationsPage.tsx

--- src/pages/NotificationsPage.tsx
+++ src/pages/NotificationsPage.tsx
@@ -7,6 +7,7 @@ import { useNotifications } from "@/contexts/NotificationsContext";
 import { useNavigate } from "react-router-dom";
 
 const NotificationsPage = () => {
+  const navigate = useNavigate();
   const { t } = useTranslation();
   const {
     notifications,
